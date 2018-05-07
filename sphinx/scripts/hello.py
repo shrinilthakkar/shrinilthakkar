01799f6329c00e8cd90c0747bf7064e7586e393d
root <root@ip-172-31-6-225.ec2.internal>
2018-05-07 12:50:43 +0000

    sphinx setup

---
+++ sphinx/scripts/hello.py
@@ -0,0 +1,12 @@
+def func1():
+    """
+    func1 inside hello.py
+    """
+    print "print statement for hello"
+
+def func2():
+    """
+    func2 inside hello.py
+    """
+    print "print statement for func2"
+
