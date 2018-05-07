bf8b51b2839c34e8cfdb1fe1db15fd68ead3076a
shrinilthakkar <35483071+shrinilthakkar@users.noreply.github.com>
2018-05-07 20:25:16 +0530

    Update conf.py

--- sphinx/conf.py
+++ sphinx/conf.py
@@ -14,7 +14,7 @@
 #
 import os
 import sys
-sys.path.append('/opt/moengage/sphinx/scripts')
+sys.path.append('../shrinilthakkar/sphinx/scripts')
 
 
 # -- Project information -----------------------------------------------------
