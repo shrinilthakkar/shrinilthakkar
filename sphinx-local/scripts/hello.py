4e07226581e5374916512bd638a619671082acaf
root <root@ip-172-31-6-225.ec2.internal>
2018-05-10 10:48:23 +0000

    sphinx local added

---
+++ sphinx-local/scripts/hello.py
@@ -0,0 +1,18 @@
+def func1():
+    """
+    Random ::::::::: func 1**************************************************************
+    """
+    print "print statement for hello"
+
+def func2():
+    """
+    func2 inside hello.py
+    """
+    print "print statement for func2"
+
+def func3():
+    """
+    func3 is here
+    """
+    print "func3 in hello.py"
+
