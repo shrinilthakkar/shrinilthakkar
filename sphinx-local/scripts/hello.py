a5ea5bbfb64a3921d9a904ef6d7cae53087459a9
shrinilthakkar <35483071+shrinilthakkar@users.noreply.github.com>
2018-05-10 16:37:30 +0530

    Update hello.py

--- sphinx-local/scripts/hello.py
+++ sphinx-local/scripts/hello.py
@@ -1,16 +1,16 @@
-def func1():
+def func_____________1():
     """
     Random ::::::::: func 1**************************************************************
     """
     print "print statement for hello"
 
-def func2():
+def func_____________2():
     """
     ##################. func2 inside hello.py
     """
     print "print statement for func2"
 
-def func3():
+def func_____________3():
     """
     $$$$$$$$$$$$$$$$$$$$$$$$$$ func3 is here
     """
