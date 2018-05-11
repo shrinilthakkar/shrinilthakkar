afbf0268d84b5a7813ee26c25ca5da857cba3dd4
shrinilthakkar <35483071+shrinilthakkar@users.noreply.github.com>
2018-05-11 15:31:58 +0530

    Update conf.py

--- sphinx-local/rst/conf.py
+++ sphinx-local/rst/conf.py
@@ -85,7 +85,7 @@ html_theme = 'nature'
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
-html_theme_options = {'classic', 'alabaster'}
+# html_theme_options = {'classic', 'alabaster'}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
