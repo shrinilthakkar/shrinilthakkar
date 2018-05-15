41c42bb72fe838654abbdd26d501fc0e7c423a13
shrinilthakkar <shrinil@moengage.com>
2018-05-15 15:06:40 +0530

    sphinx-local to sphinx

--- sphinx/scripts/hello.py
+++ sphinx/scripts/hello.py
@@ -1,12 +1,18 @@
-def func1():
+def func_____________1():
     """
-    func 1**************************************************************
+    Random ::::::::: func 1**************************************************************
     """
     print "print statement for hello"
 
-def func2():
+def func_____________2():
     """
-    func2 inside hello.py
+    ##################. func2 inside hello.py
     """
     print "print statement for func2"
+
+def func_____________3():
+    """
+    $$$$$$$$$$$$$$$$$$$$$$$$$$ func3 is here
+    """
+    print "func3 in hello.py"
 
