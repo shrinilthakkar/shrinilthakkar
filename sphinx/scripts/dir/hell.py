32be75b10c5b21522351101e58bc38020ae77222
shrinilthakkar <shrinil@moengage.com>
2018-05-17 13:33:50 +0530

    directory added in scripts

---
+++ sphinx/scripts/dir/hell.py
@@ -0,0 +1,12 @@
+def func_1():
+    """
+    This is the func 1 inside scripts folder.
+    """
+    print "print statement for hello"
+
+def func_2():
+    """
+    This is the func 2 inside scripts folder.
+    """
+    print "print statement for func2"
+
